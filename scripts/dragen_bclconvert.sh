861a240901761866dcb4411baa26f58dfc5c5bc3
your-highness <hiob@noisecore.org>
2022-04-14 15:39:39 +0200

    bclconvert: RUNID will be expanded relaxed & some logging

--- scripts/dragen_bclconvert.sh
+++ scripts/dragen_bclconvert.sh
@@ -6,44 +6,47 @@
 
 [ -z "$1" ] && { echo "Specify run ID as first argument!" >&2; exit 1; }
 RUNID=${1}
-RUNDIR=$(find /mnt/smb01-hum/NGSRawData/ /mnt/smb01-mol/NGSRawData/ -maxdepth 1 -type d -name "${RUNID}*")
-echo "[$(date)]: For ${RUNID} the following RUNDIR was found: ${RUNDIR}." >&2
+RUNDIR=$(find /mnt/smb01-hum/NGSRawData/ /mnt/smb01-mol/NGSRawData/ -maxdepth 1 -type d -name "${RUNID}")
+echo "[$(date)] For ${RUNID} the following RUNDIR was found: ${RUNDIR}." >&2
 
 DATE=${2}
 
 OUTPUTDIR=${3:-${RUNDIR}/Data/Intensities/BaseCalls/}
-echo "[$(date)]: Output directory is set as: ${OUTPUTDIR}." >&2
+echo "[$(date)] Output directory is set as: ${OUTPUTDIR}." >&2
 
-echo "[$(date)]: Correcting SampleSheet for bclconvert usage." >&2
+echo "[$(date)] Correcting SampleSheet for bclconvert usage." >&2
 
 SAMPLESHEETF="$RUNDIR/SampleSheet.csv"
 [ ! -f "$SAMPLSHEETF"] && { echo "Samplesheet $RUNDIR/SampleSheet.csv not found!" >&2;  exit 1; }
 
+cp ${SAMPLESHEETF} ${SAMPLESHEETF/.csv/_bclconvert.csv}
 if [ $(grep -c "IsIndexedRead=\"Y\"" ${RUNDIR}/RunInfo.xml) -gt 1 ]; then
+  echo "[$(date)] Dual indexing detected - Computing reverse complement for i5." >&2
   BARCODESTR="-e /\[Settings\]/ a BarcodeMismatchesIndex1,1\nBarcodeMismatchesIndex2,1"
 else
+  echo "[$(date)] Single indexing detected - No reverse complement for i5 computed." >&2
   BARCODESTR="-e /\[Settings\]/ a BarcodeMismatchesIndex1,1"
 fi
-sed \
+sed -i \
   -e "/\[Settings\]/ a CreateFastqForIndexReads,1" `#Generate FASTQ for Index sequences` \
   -e "s/Adapter,/AdapterRead1,/"                   `#Adapter not an bclconvert field - AdapterRead1 is correct` \
   -e "/^ReverseComplement,*/d"                     `#Not supported by bclconvert`\
   "$BARCODESTR"                                    `#Allow 1 Mismatch for barcode - default value` \
-  ${SAMPLESHEETF} \
-  >${SAMPLESHEETF/.csv/_bclconvert.csv}
+  ${SAMPLESHEETF/.csv/_bclconvert.csv}
 
-echo -n "Current DRAGEN LICENSE usage: " >&2
+
+echo -n "[$(date)] Current DRAGEN LICENSE usage:" >&2
 dragen_lic -f Genome | grep Gbases >&2
 
 #Check if an offset is given
 if [ -z "$DATE" ]; then
-  echo "[$(date)]: No starting time given! Processing starts now..." >&2
+  echo "[$(date)] No starting time given! Processing starts now..." >&2
 else
-  echo "[$(date)]: Starting time given!" >&2
+  echo "[$(date)] Starting time given!" >&2
   current_epoch=$(date +%s)
   target_epoch=$(date -d "$DATE" +%s)
   sleep_seconds=$(( $target_epoch - $current_epoch ))
-  echo "[$(date)]: Waiting for $sleep_seconds secs until $(date -d @${target_epoch}):" >&2
+  echo "[$(date)] Waiting for $sleep_seconds secs until $(date -d @${target_epoch}):" >&2
   echo  >&2
   c=$sleep_seconds # seconds to wait
   while [ $c -gt 0 ]; do 
@@ -58,23 +61,29 @@ fi
 COMPLETE="${RUNDIR}/CopyComplete.txt"
 if [ ! -f "$COMPLETE" ]
 then
-  echo -n "[$(date)]: Run is not yet finished, waiting..." >&2
+  echo -n "[$(date)] Run is not yet finished, waiting..." >&2
   until [ -f  "$COMPLETE" ]
   do
     sleep 5m
     echo -n "." >&2
   done
 fi
-echo "[$(date)]: Sequencing is finished." >&2
+echo "[$(date)] Sequencing is finished." >&2
 
 #NOTE: Number of threads and HW+12 option are set automatically by dragen executable but can also be specified
+echo -n "[$(date)] Starting DRAGEN BCLCONVERT:" >&2
 dragen --force --bcl-conversion-only true \
   --bcl-input-directory ${RUNDIR} \
   --sample-sheet ${SAMPLESHEETF/.csv/_bclconvert.csv} \
-  --output-directory ${OUTPUTDIR} \
+  --output-directory /staging/output/${RUNID}-fastq/ \
   --no-lane-splitting true
+echo "[$(date)] DRAGEN BCLCONVERT finished." >&2
 
-echo -n "Current DRAGEN LICENSE usage: " >&2
+echo -n "[$(date)] Copying fastq data to $OUTDIR: " >&2
+rsync -uq /staging/output/${RUNID}-fastq/ ${OUTPUTDIR} 
+echo -n "[$(date)] Copying fastq data finished." >&2
+
+echo -n "[$(date)] Current DRAGEN LICENSE usage:" >&2
 dragen_lic -f Genome | grep Gbases >&2
 
 echo "Finished." >&2
